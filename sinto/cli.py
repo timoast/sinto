b3efbab8e651d9d75e99f7f7703a5e591418ab81
timoast <timoast@users.noreply.github.com>
2019-04-11 11:15:33 -0400

    add ability to add read tags per cell

--- sinto/cli.py
+++ sinto/cli.py
@@ -1,5 +1,6 @@
 from sinto import utils
 from sinto import filterbarcodes
+from sinto import addtags
 
 
 @utils.log_info
@@ -8,5 +9,14 @@ def run_filterbarcodes(options):
     """
     filterbarcodes.filterbarcodes(
         cells=options.cells, bam=options.bam, trim_suffix=options.trim_suffix,
+        output=options.output, sam=options.sam, nproc=options.nproc, mode=options.mode
+        )
+
+@utils.log_info
+def run_addtags(options):
+    """Wraps the sctools.addtags function for use on the command line
+    """
+    addtags.addtags(
+        bam=options.bam, tagfile=options.tagfile, trim_suffix=options.trim_suffix,
         output=options.output, sam=options.sam, nproc=options.nproc, mode=options.mode
         )
