a357a49195a685546ce1dde486253e3bc6ade76d
timoast <timoast@users.noreply.github.com>
2020-02-10 17:29:08 -0500

    update code style

--- sinto/cli.py
+++ sinto/cli.py
@@ -8,15 +8,26 @@ def run_filterbarcodes(options):
     """Wraps the sctools.filterbarcodes function for use on the command line
     """
     filterbarcodes.filterbarcodes(
-        cells=options.cells, bam=options.bam, trim_suffix=options.trim_suffix,
-        output=options.output, sam=options.sam, nproc=options.nproc, mode=options.mode
-        )
+        cells=options.cells,
+        bam=options.bam,
+        trim_suffix=options.trim_suffix,
+        output=options.output,
+        sam=options.sam,
+        nproc=options.nproc,
+        mode=options.mode,
+    )
+
 
 @utils.log_info
 def run_addtags(options):
     """Wraps the sctools.addtags function for use on the command line
     """
     addtags.addtags(
-        bam=options.bam, tagfile=options.tagfile, trim_suffix=options.trim_suffix,
-        output=options.output, sam=options.sam, nproc=options.nproc, mode=options.mode
-        )
+        bam=options.bam,
+        tagfile=options.tagfile,
+        trim_suffix=options.trim_suffix,
+        output=options.output,
+        sam=options.sam,
+        nproc=options.nproc,
+        mode=options.mode,
+    )
--- sinto/utils.py
+++ sinto/utils.py
@@ -5,16 +5,20 @@ import time
 def log_info(func):
     """Decorator that prints function arguments and runtime
     """
+
     @functools.wraps(func)
     def wrapper(args):
-        print("Function {} called with the following arguments:\n".format(func.__name__))
+        print(
+            "Function {} called with the following arguments:\n".format(func.__name__)
+        )
         for arg in vars(args):
-            print(str(arg) + '\t' + str(getattr(args, arg)))
+            print(str(arg) + "\t" + str(getattr(args, arg)))
         t1 = time.time()
         func(args)
         t2 = time.time()
-        elapsed = [round(x, 2) for x in divmod(t2-t1, 60)]
+        elapsed = [round(x, 2) for x in divmod(t2 - t1, 60)]
         print("\nFunction completed in  {} m {} s\n".format(elapsed[0], elapsed[1]))
+
     return wrapper
 
 
@@ -25,8 +29,8 @@ def chunk_bam(bamfile, nproc):
     chrom_lengths = bamfile.lengths
     chunksize = sum(chrom_lengths) / int(nproc)
     intervals = []
-    for x in range(1, nproc+1):
-        position = chunksize*x
+    for x in range(1, nproc + 1):
+        position = chunksize * x
         intervals.append(find_chromosome_break(position, chrom_lengths, 0))
     return add_start_coords(intervals, chrom_lengths, bamfile)
 
@@ -36,21 +40,25 @@ def add_start_coords(intervals, chrom_lengths, bamfile):
     given the intervals that will be handled by each core,
     break into genomic regions (removing chromosome-spanning intervals)
     """
-    intervals = [[1,0]] + intervals
-    ranges = [intervals[x-1] + intervals[x] for x in range(1, len(intervals))]
+    intervals = [[1, 0]] + intervals
+    ranges = [intervals[x - 1] + intervals[x] for x in range(1, len(intervals))]
     d = {}
     x = 0
     for i in ranges:
         x += 1
         if i[0] == i[2]:
-            d[x] = [(bamfile.get_reference_name(i[0]-1), i[1], i[3])]
+            d[x] = [(bamfile.get_reference_name(i[0] - 1), i[1], i[3])]
         else:
-            d[x] = [(bamfile.get_reference_name(i[0]-1), i[1], chrom_lengths[i[0] - 1])]
+            d[x] = [
+                (bamfile.get_reference_name(i[0] - 1), i[1], chrom_lengths[i[0] - 1])
+            ]
             nchrom = i[2] - i[0]
             for y in range(nchrom - 1):
-                d[x].append((bamfile.get_reference_name(i[0] + y), 0, chrom_lengths[i[0] + y]))
-            d[x].append((bamfile.get_reference_name(i[2] -1), 0, i[3]))
-    return(d)
+                d[x].append(
+                    (bamfile.get_reference_name(i[0] + y), 0, chrom_lengths[i[0] + y])
+                )
+            d[x].append((bamfile.get_reference_name(i[2] - 1), 0, i[3]))
+    return d
 
 
 def find_chromosome_break(position, chromosomes, current_chrom):
@@ -70,7 +78,7 @@ def chunk(seq, num):
     out = []
     last = 0.0
     while last < len(seq):
-        out.append(seq[int(last):int(last + avg)])
+        out.append(seq[int(last) : int(last + avg)])
         last += avg
     return out
 
