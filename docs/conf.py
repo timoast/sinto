7c8b168eb0a24dfcaac0ba76af41f8fdb41f0ac7
timoast <timoast@users.noreply.github.com>
2020-02-17 14:24:12 -0500

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -18,11 +18,11 @@
 # -- Project information -----------------------------------------------------
 
 project = 'sinto'
-copyright = '2019, Tim Stuart'
+copyright = '2020, Tim Stuart'
 author = 'Tim Stuart'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -52,4 +52,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
