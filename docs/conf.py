af0f51c91f32d5218ac2b197c8533f6f96f7a93e
timoast <timoast@users.noreply.github.com>
2020-02-29 18:38:01 -0500

    Update version and news

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Tim Stuart'
 author = 'Tim Stuart'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.3.2'
 
 
 # -- General configuration ---------------------------------------------------
